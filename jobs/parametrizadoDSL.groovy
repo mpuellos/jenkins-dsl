f20e9f6127bdf6a8f96ac37d4753fadb5026d01e
puellomonica <87338501+puellomonica@users.noreply.github.com>
2025-10-30 21:19:36 -0500

    feat: add new job

--- jobs/parametrizadoDSL.groovy
+++ jobs/parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job('ejemplo2-job-DSL') {
+job('ejemplo3-job-DSL') {
     description('Job DSL de ejemplo para el curso de Jenkins')
     scm {
         git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->

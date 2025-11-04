96c8814119e4e1920bcfa2cf6487e47631a5b208
puellomonica <87338501+puellomonica@users.noreply.github.com>
2025-11-04 17:31:30 -0500

    feat: update multibranchpipeline class

--- jobs/multibranch_pipeline_uno.groovy
+++ jobs/multibranch_pipeline_uno.groovy
@@ -1,8 +1,7 @@
-import MultibranchPipeline
 
 String project_name = "multibranch-pipeline-uno"
 
 new MultibranchPipeline().
     project_name(project_name).
-    repository_name(project_name).
+    repository_name(project_nam).
     build(this)

b651d2a67aea65503690d428721a3d4397681fe6
puellomonica <87338501+puellomonica@users.noreply.github.com>
2025-12-15 17:52:52 -0500

    update config

--- jobs/multibranch_pipeline_uno.groovy
+++ jobs/multibranch_pipeline_uno.groovy
@@ -1,5 +1,8 @@
 String project_name = "simple-java-maven-app"
 
+println "Tipo de this: ${this.class}"
+println "Es DslFactory?: ${this instanceof javaposse.jobdsl.dsl.DslFactory}"
+
 new MultibranchPipeline().
         project_name(project_name).
         repository_name(project_name).
